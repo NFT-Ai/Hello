4bc44efed24f89a4e3845ec622b2bdf9f5c0507e
NFT-Ai <108221782+NFT-Ai@users.noreply.github.com>
2025-03-17 13:50:11 +0100

    Add Sanity schema with Page document type

---
+++ sanity.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "sanity"
+import { deskTool } from "sanity/desk"
+import { visionTool } from "@sanity/vision"
+import { schemaTypes } from "./sanity/schemas"
+
+export default defineConfig({
+  name: "default",
+  title: "Hello",
+
+  projectId: "aldx01rl",
+  dataset: "production",
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
